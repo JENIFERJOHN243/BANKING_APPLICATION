dbc9a47ec03a23362055f75abda4c472fe91e417
JENIFERJOHN243 <112951358+JENIFERJOHN243@users.noreply.github.com>
2022-10-09 17:53:13 +0530

    Update BankingApplication.java

--- BankingApplication/src/controller/BankingApplication.java
+++ BankingApplication/src/controller/BankingApplication.java
@@ -1,8 +1,10 @@
 package controller;
 
+import java.sql.SQLException;
+
 public class BankingApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		BankingController customer = new BankingController();
 		customer.startProcess();
 	}
